333ff846a84744638a10b65fc5815df00077f7b7
menkam <menkam35@gmail.com>
2018-06-06 15:41:55 +0100

    modifiaction de la vue de l'admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,18 +2,19 @@
 @if(\Auth::user()->hasRole('admin'))
 @section('page_content')
     <div id="contener">
-        <h1>page d'accueil de l'administrateur</h1>
+        <h6>page d'accueil de l'administrateur</h6>
         <div id="heure">voici l'heure du serveur</div>
     </div>
 @endsection
 
 @section('scripts')
     <script type="text/javascript">
-        alert(dateCourante);
-        alert(heureCourante);
+        //alert(dateCourante);
+        //alert(heureCourante);
 
         dateCourante = dateCourante;
         heureCourante = heureCourante;
+        $("#heure").html("<h1>nous somme "+dateCourante+" et il est "+heureCourante+"</h1>");
     </script>
 
 @endsection
