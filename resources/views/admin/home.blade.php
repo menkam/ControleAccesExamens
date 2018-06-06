1264de83f343f6d188f4f8ec722e0ba5af917614
menkam <menkam35@gmail.com>
2018-06-06 15:33:13 +0100

    modifiaction de la vue de l'admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,10 +3,18 @@
 @section('page_content')
     <div id="contener">
         <h1>page d'accueil de l'administrateur</h1>
+        <div id="heure">voici l'heure du serveur</div>
     </div>
 @endsection
 
 @section('scripts')
+    <script type="text/javascript">
+        alert(dateCourante);
+        alert(heureCourante);
+
+        dateCourante = dateCourante;
+        heureCourante = heureCourante;
+    </script>
 
 @endsection
 
